baa3d8ce5aecb9077fd19eb2774564d1e2a16a3b
mixalot2009 <mixalot2009@gmail.com>
2015-02-12 21:28:19 -0500

    School Assignment 2
    assignment

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+var weekOne = prompt("To calculate your average hours enter hours worked in week 1:")
+var weekTwo = prompt("Enter hours worked in: week 2")
+var weekThree = prompt("Enter hours worked in week 3:")
+var weekFour = prompt("Enter hours worked in week 4:")
+var averageHoursMonth1 = week1 + week2 + week3 + week4
+alert("your monthly hours worked for month 1 is" + average(averageHoursMonth1)
+var weekFive = prompt("Enter hours worked in week 5:")
+var weekSix = prompt("Enter hours worked in week 6:")
+var weekSeven = prompt("Enter hours worked in week 7:")
+var weekEight = prompt("Enter hours worked in week 8:")
+var averageHoursMonth2 = week5 + week6 + week7 + week8
+alert(“Your monthly hours worked for month 2 is” + average(averageHoursMonth2)
+alert(“Your projected hours for this month is” + averageHoursMonth1 * 12 + "hours")
